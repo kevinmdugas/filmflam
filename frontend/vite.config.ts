2070bcdfb0d2d549f8b2316a2846b301b79a98a7
dev <kdugas@pdx.edu>
2023-05-26 21:54:55 -0700

    Initial frontend setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+// @ts-ignore
+import * as envVars from "./.env.ts";
+
+const define: Record< string, string | undefined > = {};
+
+for (const [key, value] of Object.entries(envVars)){
+  define[`process.env.${key}`] = JSON.stringify(value);
+}
+
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [react(), tsconfigPaths()],
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./test/setup.ts",
+    },
+    // vite config
+    define,
+  };
+});
