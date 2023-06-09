e4a61804ca286f749c7a2aafd73e790bc5a39e58
dev <kdugas@pdx.edu>
2023-06-09 15:27:16 -0700

    Tweak nav and colors

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -4,34 +4,31 @@ import {Home} from "@/components/Home.tsx";
 export function FilmFlamRoutes() {
     return (
         <>
-            <nav className="navbar fixed-top navbar-expand-lg bg-body-secondary">
+            <nav className="navbar fixed-top navbar-expand bg-body-secondary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand text-title" to="/">FilmFlam </Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
+                    <div>
+                        <Link className="navbar-brand text-title fs-1 fw-bold fst-italic" to="/">FilmFlam </Link>
+                    </div>
                     <div className="collapse navbar-collapse justify-content-end" id="main-nav">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <button className="bg-body-secondary">
-                                    <Link className="text-primary" to="/">Home </Link>
+                                <button>
+                                    <Link className="text-primary text-decoration-none" to="/">Home </Link>
                                 </button>
                             </li>
                             <li className="nav-item">
-                                <button className="bg-body-secondary">
-                                    <Link className="text-primary" to="/login"> Log In </Link>
+                                <button>
+                                    <Link className="text-primary text-decoration-none" to="/login"> Log In </Link>
                                 </button>
                             </li>
                             <li className="nav-item">
-                                <button className="bg-body-secondary">
-                                    <Link className="text-primary" to="/signup"> Sign Up </Link>
+                                <button>
+                                    <Link className="text-primary text-decoration-none" to="/signup"> Sign Up </Link>
                                 </button>
                             </li>
                             <li className="nav-item">
-                                <button className="bg-body-secondary">
-                                    <Link className="text-primary" to="/about"> About </Link>
+                                <button>
+                                    <Link className="text-primary text-decoration-none" to="/about"> About </Link>
                                 </button>
                             </li>
                         </ul>
