f07f5ae1b6a8034d4f54a83609164bdda66304d1
dev <kdugas@pdx.edu>
2023-06-10 15:17:54 -0700

    Add new title migration

---
+++ backend/src/db/migrations/Migration20230610220125_TitleGenreYear.ts
@@ -0,0 +1,14 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20230610220125_TitleGenreYear extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table "title" add column "year" varchar(255) not null, add column "genres" text[] not null;');
+  }
+
+  async down(): Promise<void> {
+    this.addSql('alter table "title" drop column "year";');
+    this.addSql('alter table "title" drop column "genres";');
+  }
+
+}
